206b33343f7dd8c7717ff9ed3995b17cd2d999ae
Richard Campbell <rlcamp@users.noreply.github.com>
2025-05-16 18:51:34 -0700

    initial commit

---
+++ picosdio.c
@@ -0,0 +1,326 @@
+#include "pico/stdio_uart.h"
+#include "hardware/clocks.h"
+#include "hardware/irq.h"
+#include "hardware/uart.h"
+#include "hardware/dma.h"
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+#include "RP2350.h"
+
+#include <stdio.h>
+
+void yield(void) {
+    __DSB(); __WFE();
+}
+
+static void cs_low(void) {
+    gpio_put(15, 0);
+}
+
+static void cs_high(void) {
+    gpio_put(15, 1);
+}
+
+static uint8_t spi_receive_one_byte_with_rx_enabled(void) {
+    uint8_t ret = 0;
+    spi_read_blocking(spi1, 0xFF, &ret, 1);
+    return ret;
+}
+
+static uint8_t r1_response(void) {
+    uint8_t result, attempts = 0;
+    /* sd and mmc agree on max 8 attempts, mmc requires at least two attempts */
+    while (0xFF == (result = spi_receive_one_byte_with_rx_enabled()) && attempts++ < 8);
+
+    return result;
+}
+
+static unsigned char crc7_left_shifted(const unsigned char * restrict const message, const size_t length) {
+    const unsigned char polynomial = 0b10001001;
+    unsigned char crc = 0;
+
+    for (size_t ibyte = 0; ibyte < length; ibyte++) {
+        crc ^= message[ibyte];
+
+        for (size_t ibit = 0; ibit < 8; ibit++)
+            crc = (crc & 0x80u) ? (crc << 1) ^ (polynomial << 1) : (crc << 1);
+    }
+
+    return crc & 0xfe;
+}
+
+static void send_command_with_crc7(const uint8_t cmd, const uint32_t arg) {
+    unsigned char msg[6] = { cmd | 0x40, arg >> 24, arg >> 16, arg >> 8, arg, 0x01 };
+    msg[5] |= crc7_left_shifted(msg, 5);
+
+    spi_write_blocking(spi1, msg, 6);
+}
+
+static uint8_t command_and_r1_response(const uint8_t cmd, const uint32_t arg) {
+    send_command_with_crc7(cmd, arg);
+    return r1_response();
+}
+
+static void wait_for_card_ready(void) {
+    uint8_t ret;
+    do spi_read_blocking(spi1, 0xFF, &ret, 1);
+    while (ret != 0xFF);
+}
+
+static uint32_t spi_receive_uint32be(void) {
+    uint32_t ret;
+    spi_read_blocking(spi1, 0xFF, (void *)&ret, 4);
+    return __builtin_bswap32(ret);
+}
+
+int spi_sd_init(void) {
+    spi_init(spi1, 400000);
+    gpio_set_function(10, GPIO_FUNC_SPI);
+    gpio_set_function(11, GPIO_FUNC_SPI);
+    gpio_set_function(12, GPIO_FUNC_SPI);
+
+    gpio_init(15);
+    gpio_set_dir(15, GPIO_OUT);
+    cs_high();
+
+    /* clear miso */
+    cs_low();
+    spi_write_blocking(spi1, &(uint8_t){ 0xFF }, 1);
+    cs_high();
+
+    /* and then clock out at least 74 cycles at 100-400 kBd with cs pin held high */
+    spi_write_blocking(spi1, (unsigned char[10]) { [0 ... 9] = 0xFF }, 10);
+
+    /* cmd0, software reset. TODO: is looping this even necessary? */
+    for (size_t ipass = 0;; ipass++) {
+        /* if card likely not present, give up */
+        if (ipass > 1024) {
+            dprintf(2, "%s(%d): fail\r\n", __func__, __LINE__);
+            return -1;
+        }
+        cs_low();
+
+        /* send cmd0 */
+        const uint8_t cmd0_r1_response = command_and_r1_response(0, 0);
+        cs_high();
+
+        if (0x01 == cmd0_r1_response) break;
+
+        /* give other stuff a chance to run if we are looping */
+        __SEV(); yield();
+    }
+
+    dprintf(2, "%s: cmd0 success\r\n", __func__);
+
+    /* cmd8, check voltage range and test pattern */
+    for (size_t ipass = 0;; ipass++) {
+        if (ipass > 3) {
+            /* TODO: find out how many times we should try this before giving up */
+            spi_deinit(spi1);
+            return -1;
+        }
+        cs_low();
+        wait_for_card_ready();
+
+        const uint8_t r1_response = command_and_r1_response(8, 0x1AA);
+
+        if (0x1 != r1_response) {
+            cs_high();
+            continue;
+        }
+
+        const uint32_t response = spi_receive_uint32be();
+        cs_high();
+
+        if (0x1AA == response) break;
+    }
+
+    dprintf(2, "%s: cmd8 success\r\n", __func__);
+
+    /* cmd59, re-enable crc feature, which is disabled by cmd0 */
+    cs_low();
+    wait_for_card_ready();
+    if (command_and_r1_response(59, 1) > 1) {
+        cs_high();
+        spi_deinit(spi1);
+        return -1;
+    }
+    cs_high();
+
+    dprintf(2, "%s: cmd59 success\r\n", __func__);
+
+    /* cmd55, then acmd41, init. must loop this until the response is 0 */
+    for (size_t ipass = 0;; ipass++) {
+        if (ipass > 400000 / (8 * 20)) {
+            /* each pass takes about 20 bytes minimum, give up after one second */
+            spi_deinit(spi1);
+            return -1;
+        }
+        cs_low();
+        wait_for_card_ready();
+
+        const uint8_t cmd55_r1_response = command_and_r1_response(55, 0);
+        cs_high();
+
+        if (cmd55_r1_response > 1) continue;
+
+        cs_low();
+        wait_for_card_ready();
+
+        const uint8_t acmd41_r1_response = command_and_r1_response(41, 1U << 30);
+        cs_high();
+
+        if (!acmd41_r1_response) break;
+    }
+
+    dprintf(2, "%s: cmd55+acmd41 success\r\n", __func__);
+
+    spi_deinit(spi1);
+    const unsigned actual_baud = spi_init(spi1, 24000000);
+    dprintf(2, "%s: baud rate set to %u\r\n", __func__, actual_baud);
+
+    /* TODO: if any of the following fail, restart the procedure with a lower baud rate */
+    do {
+        /* cmd58, read ocr register */
+        cs_low();
+        wait_for_card_ready();
+        if (command_and_r1_response(58, 0) > 1) break;
+
+        const unsigned int ocr = spi_receive_uint32be();
+        (void)ocr;
+        cs_high();
+
+        /* cmd16, set block length to 512 */
+        cs_low();
+        wait_for_card_ready();
+        if (command_and_r1_response(16, 512) > 1) break;
+        cs_high();
+
+        /* we get here on overall success of this function */
+        cs_high();
+        spi_deinit(spi1);
+
+        dprintf(2, "%s: success\r\n", __func__);
+        return 0;
+    } while(0);
+
+    /* we get here on failure */
+    cs_high();
+    spi_deinit(spi1);
+    return -1;
+}
+
+int spi_sd_read_blocks(void * buf, unsigned long blocks, unsigned long long block_address) {
+    spi_init(spi1, 24000000);
+    cs_low();
+    wait_for_card_ready();
+
+    /* send cmd17 or cmd18 */
+    if (command_and_r1_response(blocks > 1 ? 18 : 17, block_address) != 0) {
+        cs_high();
+        spi_deinit(spi1);
+        dprintf(2, "%s(%d): fail\r\n", __func__, __LINE__);
+        return -1;
+    }
+
+    /* clock out the response in 1 + 512 + 2 byte blocks */
+    for (size_t iblock = 0; iblock < blocks; iblock++) {
+        uint8_t result;
+        /* this can loop for a while */
+        while (0xFF == (result = spi_receive_one_byte_with_rx_enabled()));
+
+        /* when we break out of the above loop, we've read the Data Token byte */
+        if (0xFE != result) {
+            cs_high();
+            spi_deinit(spi1);
+            dprintf(2, "%s(%d): fail\r\n", __func__, __LINE__);
+            return -1;
+        }
+
+        unsigned char * restrict const block = ((unsigned char *)buf) + 512 * iblock;
+
+        const uint dma_rx = dma_claim_unused_channel(true);
+        const uint dma_tx = dma_claim_unused_channel(true);
+
+        /* there has got to be a better way to do this than clock out bytes */
+        /* TODO: at least change these to 16 bit transactions */
+        dma_channel_config cfg = dma_channel_get_default_config(dma_tx);
+        channel_config_set_transfer_data_size(&cfg, DMA_SIZE_8);
+        channel_config_set_dreq(&cfg, spi_get_dreq(spi1, true));
+        channel_config_set_read_increment(&cfg, false);
+        dma_channel_configure(dma_tx, &cfg, &spi_get_hw(spi1)->dr, &(uint8_t) { 0xFF }, 512, false);
+
+        cfg = dma_channel_get_default_config(dma_rx);
+        channel_config_set_transfer_data_size(&cfg, DMA_SIZE_8);
+        channel_config_set_dreq(&cfg, spi_get_dreq(spi1, false));
+        channel_config_set_read_increment(&cfg, false);
+        channel_config_set_write_increment(&cfg, true);
+        dma_channel_configure(dma_rx, &cfg, block, &spi_get_hw(spi1)->dr, 512, false);
+
+        /* compute a CCITT16 CRC on the bytes flowing through the rx dma */
+        dma_sniffer_enable(dma_rx, 0x2, true);
+        dma_sniffer_set_data_accumulator(0);
+
+        /* start both dma channels simulataneously */
+        dma_start_channel_mask((1u << dma_tx) | (1u << dma_rx));
+
+        /* do other things while waiting for dma to finish */
+        /* TODO: enable interrupt and wait for it */
+        while (dma_channel_is_busy(dma_rx)) { __SEV(); yield(); }
+
+        uint16_t crc_dma = dma_sniffer_get_data_accumulator();
+
+        dma_channel_unclaim(dma_rx);
+        dma_channel_unclaim(dma_tx);
+        dma_sniffer_disable();
+
+        uint16_t crc_swapped;
+        spi_read_blocking(spi1, 0xFF, (void *)&crc_swapped, 2);
+        const uint16_t crc_received = __builtin_bswap16(crc_swapped);
+
+        dprintf(2, "%s: received crc 0x%04X, dma 0x%04X\r\n", __func__, crc_received, crc_dma);
+
+        if (crc_received != crc_dma) {
+            cs_high();
+            spi_deinit(spi1);
+            dprintf(2, "%s: bad crc\r\n", __func__);
+            return -1;
+        }
+    }
+
+    /* if we sent cmd18, send cmd12 to stop */
+    if (blocks > 1) {
+        send_command_with_crc7(12, 0);
+
+        /* CMD12 wants an extra byte prior to the response */
+        spi_write_blocking(spi1, (unsigned char[1]) { 0xff }, 1);
+
+        (void)r1_response();
+        wait_for_card_ready();
+    }
+
+    cs_high();
+    spi_deinit(spi1);
+
+    return 0;
+}
+
+int main(void) {
+    set_sys_clock_48mhz();
+
+    stdio_uart_init();
+    dprintf(2, "hello world stderr\r\n");
+
+    spi_sd_init();
+    static unsigned char buf[512];
+    spi_sd_read_blocks(buf, 1, 0);
+
+    for (size_t ia = 0; ia < 32; ia++) {
+        for (size_t ib = 0; ib < 16; ib++)
+            dprintf(2, " %04X", buf[ib + 16 * ia]);
+        dprintf(2, "\r\n");
+    }
+
+    while (1) yield();
+    NVIC_SystemReset();
+}
