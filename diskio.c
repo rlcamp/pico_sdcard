6a710fe05be136a063d71b3bbfd69f40f41b0cee
Richard Campbell <rlcamp@users.noreply.github.com>
2025-08-16 16:50:12 -0700

    add fatfs

---
+++ diskio.c
@@ -0,0 +1,146 @@
+/* minimum viable glue code between fatfs and samd51 sercom spi sd code */
+
+/* definitions of datatypes and of functions expected by ff.c */
+#include "ff.h"
+#include "diskio.h"
+
+/* block device implementation code being wrapped by this */
+#include "rp2350_sdcard.h"
+
+/* needed for INT_MAX, this will go away */
+#include <limits.h>
+
+#include <stdio.h>
+
+size_t fatfs_sectors_read = 0, fatfs_sectors_written = 0;
+
+unsigned char verbose = 2;
+
+unsigned char diskio_initted = 0;
+
+DSTATUS disk_status(BYTE pdrv) {
+    (void)pdrv;
+    return diskio_initted ? 0 : STA_NOINIT;
+}
+
+static LBA_t block_cache_sectors[64];
+static unsigned char block_cache[64][512];
+static const size_t B = sizeof(block_cache) / sizeof(block_cache[0]);
+static size_t icache = 0;
+
+DSTATUS disk_initialize(BYTE pdrv) {
+    (void)pdrv;
+    if (!diskio_initted) {
+        if (-1 == spi_sd_init()) return STA_NOINIT;
+    }
+
+    __builtin_memset(block_cache_sectors, 0, sizeof(block_cache_sectors));
+
+    diskio_initted = 1;
+    return 0;
+}
+
+static LBA_t deferred_zeros_sector_start = 0;
+static UINT deferred_zeros_sector_count = 0;
+
+static DRESULT flush_deferred_zeros(void) {
+    const UINT count = deferred_zeros_sector_count;
+    deferred_zeros_sector_count = 0;
+
+    fatfs_sectors_written += count;
+
+    if (-1 == spi_sd_write_blocks(NULL, count, deferred_zeros_sector_start))
+        return RES_ERROR;
+    return 0;
+}
+
+static void cache_block(const BYTE * buff, LBA_t sector) {
+    for (size_t icache_search = 0; icache_search < B; icache_search++)
+        if (sector && block_cache_sectors[icache_search] == sector) {
+            icache = icache_search;
+            break;
+        }
+
+    __builtin_memcpy(block_cache[icache], buff, 512);
+    block_cache_sectors[icache] = sector;
+    icache = (icache + 1) % B;
+}
+void print_block(const unsigned char buf[]);
+
+DRESULT disk_read(BYTE pdrv, BYTE * buff, LBA_t sector, UINT count) {
+    (void)pdrv;
+
+    if (deferred_zeros_sector_count) {
+        const DRESULT res = flush_deferred_zeros();
+        if (res) return res;
+    }
+
+    for (size_t icache_search = 0; icache_search < B; icache_search++)
+        if (1 == count && sector && block_cache_sectors[icache_search] == sector) {
+            if (verbose >= 2)
+                dprintf(2, "%s(%d): reusing cached block %u at %u\r\n", __func__, __LINE__, (unsigned)sector, icache_search);
+            __builtin_memcpy(buff, block_cache[icache_search], 512);
+            return 0;
+        }
+
+    if (verbose >= 2)
+        dprintf(2, "%s(%d): reading %u blocks starting at %u\r\n", __func__, __LINE__, count, (unsigned)sector);
+
+    fatfs_sectors_read += count;
+
+    /* this will block, but will internally call yield() and __WFI() */
+    if (-1 == spi_sd_read_blocks(buff, count, sector)) return RES_ERROR;
+
+    cache_block(buff, sector);
+    return 0;
+}
+
+static int buffer_points_to_all_zeros(const BYTE * buff, UINT count) {
+    for (size_t ibyte = 0; ibyte < 512 * count; ibyte++)
+        if (buff[ibyte]) return 0;
+    return 1;
+}
+
+DRESULT disk_write(BYTE pdrv, const BYTE * buff, LBA_t sector, UINT count) {
+    (void)pdrv;
+
+    if (!deferred_zeros_sector_count || sector == deferred_zeros_sector_start + deferred_zeros_sector_count) {
+        if (buffer_points_to_all_zeros(buff, count)) {
+            if (!deferred_zeros_sector_count)
+                deferred_zeros_sector_start = sector;
+            deferred_zeros_sector_count += count;
+            return 0;
+        }
+    }
+    else if (deferred_zeros_sector_count) {
+        const DRESULT res = flush_deferred_zeros();
+        if (res) return res;
+    }
+
+    if (verbose >= 2)
+        dprintf(2, "%s(%d): writing block(s) starting at %u\r\n", __func__, __LINE__, (unsigned)sector);
+
+    fatfs_sectors_written += count;
+
+    if (-1 == spi_sd_write_blocks(buff, count, sector)) return RES_ERROR;
+
+    cache_block(buff, sector);
+    return 0;
+}
+
+DRESULT disk_ioctl(BYTE pdrv, BYTE cmd, void * buff) {
+    (void)pdrv;
+    if (CTRL_SYNC == cmd) {
+        if (deferred_zeros_sector_count) {
+            const DRESULT res = flush_deferred_zeros();
+            if (res) return res;
+        }
+        return 0;
+    }
+    else if (GET_BLOCK_SIZE == cmd)
+        *(LBA_t *)buff = 1; /* TODO: populate this from actual */
+    else if (GET_SECTOR_COUNT == cmd)
+        *(LBA_t *)buff = INT_MAX; /* TODO: populate this from actual */
+    else return RES_PARERR;
+    return 0;
+}
--- pico_sdcard.c
+++ pico_sdcard.c
@@ -4,21 +4,63 @@
 #include "hardware/uart.h"
 #include "hardware/gpio.h"
 #include "RP2350.h"
-
+#include "ff.h"
 #include "rp2350_sdcard.h"
 
 #include <stdio.h>
+#include <string.h>
+
+static void set_first_value_in_string(char buf[], unsigned value) {
+    char * cursor = buf;
+    while (cursor[1] >= '0' && cursor[1] <= '9') cursor++;
+    while (value && cursor >= buf) {
+        *(cursor--) = '0' + (value % 10);
+        value /= 10;
+    }
+    while (cursor >= buf)
+        *(cursor--) = '0';
+}
+
+static int fputs_to_open_file(FIL * fp, const char * string) {
+    const size_t len = strlen(string);
+    FRESULT fres;
+    UINT write_count;
+    if ((fres = f_write(fp, string, len, &write_count))) {
+        dprintf(2, "error: %s: f_write(): %d\r\n", __func__, fres);
+        return -1;
+    }
+    if (write_count < len) {
+        dprintf(2, "error: %s: short write\r\n", __func__);
+        return -1;
+    }
+    return 0;
+}
 
 void yield(void) {
     __DSB(); __WFE();
 }
 
-static void print_block(const unsigned char buf[]) {
-    for (size_t ia = 0; ia < 32; ia++) {
-        for (size_t ib = 0; ib < 16; ib++)
-            dprintf(2, " %02X", buf[ib + 16 * ia]);
-        dprintf(2, "\r\n");
+__attribute((aligned(4))) static FATFS * fs = &(static FATFS) { };
+__attribute((aligned(4))) static FIL * fp = &(static FIL) { };
+
+int ls(void) {
+    FRESULT fres;
+    DIR dir;
+    FILINFO info;
+
+    if ((fres = f_opendir(&dir, "")) != FR_OK) {
+        dprintf(2, "error: %s: f_opendir(): %d\r\n", __func__, fres);
+        return -1;
     }
+
+    while (FR_OK == f_readdir(&dir, &info) && info.fname[0] != '\0') {
+        dprintf(2, "%s\r\n", info.fname);
+        __SEV();
+        yield();
+    }
+
+    f_closedir(&dir);
+    return 0;
 }
 
 int main(void) {
@@ -36,18 +78,48 @@ int main(void) {
     dprintf(2, "hello world stderr\r\n");
 
     do {
-        if (-1 == spi_sd_init()) break;
-        static unsigned char buf[512];
-        if (-1 == spi_sd_read_blocks(buf, 1, 0)) break;
-        print_block(buf);
+        FRESULT fres;
+        if ((fres = f_mount(fs, "", 1))) {
+            if (FR_NOT_READY == fres)
+                dprintf(2, "error: %s: card apparently not present\r\n", __func__);
+            else
+                dprintf(2, "error: %s: f_mount(): %d\r\n", __func__, fres);
+            break;
+        }
+        dprintf(2, "%s: mounted\r\n", __func__);
+        if (-1 == ls()) break;
 
-        if (-1 == spi_sd_read_blocks(buf, 1, 1)) break;
-        print_block(buf);
+        /* loop until we get to the first available filename */
+        unsigned file_id = 0;
+        char path[] = "000000.txt";
 
-        if (-1 == spi_sd_write_blocks(buf, 1, 1)) break;
+        while (1) {
+            set_first_value_in_string(path, file_id);
+            fres = f_open(fp, path, FA_CREATE_NEW | FA_WRITE);
+            if (FR_OK == fres)
+                break;
+            if (FR_EXIST == fres) {
+                file_id++;
+                continue;
+            } else {
+                dprintf(2, "%s: f_open(\"%s\"): %d\r\n", __func__, path, fres);
+                break;
+            }
+        }
 
-        if (-1 == spi_sd_read_blocks(buf, 1, 1)) break;
-        print_block(buf);
+        if (fres != FR_OK) break;
+
+        dprintf(2, "%s: opened \"%s\"\r\n", __func__, path);
+
+        if (-1 == fputs_to_open_file(fp, "hello\n")) break;
+
+        if ((fres = f_close(fp))) {
+            dprintf(2, "%s: f_close(\"%s\"): %d\r\n", __func__, path, fres);
+            break;
+        }
+
+        dprintf(2, "%s: done\r\n", __func__);
+
     } while(0);
 
     gpio_put(22, 0);
