556d4a3a6bc7c0b61e63a95f4e8d78a0f6d3bce5
Richard Campbell <rlcamp@users.noreply.github.com>
2025-08-16 13:10:33 -0700

    move functionality to module

---
+++ rp2350_sdcard.h
@@ -0,0 +1,6 @@
+int spi_sd_read_blocks(void * buf, unsigned long blocks, unsigned long long block_address);
+int spi_sd_write_blocks(const void * buf, const unsigned long blocks, const unsigned long long block_address);
+
+int spi_sd_write_pre_erase(unsigned long blocks);
+int spi_sd_init(void);
+
