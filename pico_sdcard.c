e7f00b4c04854009d7eb641a7784bbd90b5b0eff
Richard Campbell <rlcamp@users.noreply.github.com>
2025-08-16 17:32:33 -0700

    shut down clocks to a bunch of peripherals we don't need

--- pico_sdcard.c
+++ pico_sdcard.c
@@ -170,6 +170,39 @@ int main(void) {
 //    set_sys_clock_hz(96000000, true);
     run_from_xosc();
 
+    scb_hw->scr |= M33_SCR_SLEEPDEEP_BITS;
+
+    /* turn off clocks to a bunch of stuff we aren't (yet) using */
+    clocks_hw->wake_en1 = (CLOCKS_WAKE_EN1_BITS &
+                           ~(CLOCKS_WAKE_EN1_CLK_USB_BITS|
+                             CLOCKS_WAKE_EN1_CLK_SYS_USBCTRL_BITS |
+                             CLOCKS_WAKE_EN1_CLK_SYS_UART1_BITS |
+                             CLOCKS_WAKE_EN1_CLK_PERI_UART1_BITS |
+                             CLOCKS_WAKE_EN1_CLK_SYS_TRNG_BITS |
+                             CLOCKS_WAKE_EN1_CLK_SYS_SPI0_BITS |
+                             CLOCKS_WAKE_EN1_CLK_PERI_SPI0_BITS));
+
+    clocks_hw->wake_en0 = (CLOCKS_WAKE_EN0_BITS &
+                           ~(CLOCKS_WAKE_EN0_CLK_SYS_SHA256_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_PWM_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_PLL_USB_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_PLL_SYS_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_PIO2_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_PIO1_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_PIO0_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_JTAG_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_I2C1_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_I2C0_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_HSTX_BITS |
+                             CLOCKS_WAKE_EN0_CLK_HSTX_BITS |
+                             CLOCKS_WAKE_EN0_CLK_SYS_ADC_BITS |
+                             CLOCKS_WAKE_EN0_CLK_ADC_BITS));
+
+    /* make sure we don't clock anything in sleep that wasn't clocked in wake */
+    clocks_hw->sleep_en1 = clocks_hw->wake_en1;
+    clocks_hw->sleep_en0 = clocks_hw->wake_en0;
+
+
     gpio_init(22);
     gpio_set_dir(22, GPIO_OUT);
     gpio_put(22, 1);
