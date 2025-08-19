7924e3272215c9b2d20dfb42d861125a40a424e4
Richard Campbell <rlcamp@users.noreply.github.com>
2025-08-19 07:35:13 -0700

    rework error handling

--- rp2350_tsys01.c
+++ rp2350_tsys01.c
@@ -43,39 +43,39 @@ int tsys01_init(void) {
 
 int tsys01_read(long * temp_thousandths_p) {
     i2c_request();
-    if (-1 == i2c_write_blocking(i2c0, 0x77, &(uint8_t){ 0x48 }, 1, false)) {
-        i2c_release();
-        return -1;
-    }
 
-    i2c_unlock();
-    lower_power_sleep_ms(10);
-    i2c_lock();
+    do {
+        if (-1 == i2c_write_blocking(i2c0, 0x77, &(uint8_t){ 0x48 }, 1, false)) break;
 
-    unsigned char bytes[3];
-    if (-1 == i2c_write_blocking(i2c0, 0x77, &(uint8_t){ 0x00 }, 1, false) ||
-        -1 == i2c_read_blocking(i2c0, 0x77, bytes, 3, false)) {
+        i2c_unlock();
+        lower_power_sleep_ms(10);
+        i2c_lock();
+
+        unsigned char bytes[3];
+        if (-1 == i2c_write_blocking(i2c0, 0x77, &(uint8_t){ 0x00 }, 1, false) ||
+            -1 == i2c_read_blocking(i2c0, 0x77, bytes, 3, false)) break;
+
         i2c_release();
-        return -1;
-    }
+
+        const uint32_t adc24 = (bytes[0] << 16) | (bytes[1] << 8) | bytes[2];
+
+        /* TODO: this is literal from the datasheet, if possible rework all this sketchy
+         math to return temperature in thousandths of a deg without doing floating point */
+        const float adc16 = adc24 / 256.0f;
+        const float adc16_2 = adc16 * adc16;
+        const float adc16_3 = adc16_2 * adc16;
+        const float adc16_4 = adc16_2 * adc16_2;
+
+        const float temp = (-2.0f * coefficients[4] * 1e-21f * adc16_4 +
+                            +4.0f * coefficients[3] * 1e-16f * adc16_3 +
+                            -2.0f * coefficients[2] * 1e-11f * adc16_2 +
+                            +1.0f * coefficients[1] *  1e-6f * adc16 +
+                            -1.5f * coefficients[0] *  1e-2f);
+        *temp_thousandths_p = lrintf(temp * 1e3f);
+        return 0;
+
+    } while(0);
 
     i2c_release();
-
-    const uint32_t adc24 = (bytes[0] << 16) | (bytes[1] << 8) | bytes[2];
-
-    /* TODO: this is literal from the datasheet, if possible rework all this sketchy math
-     to return temperature in thousandths of a degree without doing any floating point */
-    const float adc16 = adc24 / 256.0f;
-    const float adc16_2 = adc16 * adc16;
-    const float adc16_3 = adc16_2 * adc16;
-    const float adc16_4 = adc16_2 * adc16_2;
-
-    const float temp = (-2.0f * coefficients[4] * 1e-21f * adc16_4 +
-                        +4.0f * coefficients[3] * 1e-16f * adc16_3 +
-                        -2.0f * coefficients[2] * 1e-11f * adc16_2 +
-                        +1.0f * coefficients[1] *  1e-6f * adc16 +
-                        -1.5f * coefficients[0] *  1e-2f);
-    *temp_thousandths_p = lrintf(temp * 1e3f);
-    return 0;
+    return -1;
 }
-
