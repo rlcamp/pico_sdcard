1ebb978bcf869543cc9e5de02003889d5ba2f704
Richard Campbell <rlcamp@users.noreply.github.com>
2025-08-28 20:02:51 -0700

    add missing prototypes to header

--- rp2350_sdcard.h
+++ rp2350_sdcard.h
@@ -4,3 +4,7 @@ int spi_sd_write_blocks(const void * buf, const unsigned long blocks, const unsi
 int spi_sd_write_pre_erase(unsigned long blocks);
 int spi_sd_init(void);
 
+int spi_sd_write_pre_erase(unsigned long blocks);
+int spi_sd_write_blocks_start(unsigned long long block_address);
+int spi_sd_write_some_blocks(const void * buf, const unsigned long blocks);
+void spi_sd_write_blocks_end(void);
